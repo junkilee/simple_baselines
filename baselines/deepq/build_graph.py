02e98cc8ee252df46b5757b04b21eb9033693f22
Kevin Du <kevin_du@brown.edu>
2019-02-25 12:47:22 -0500

    added build_act for training with expectation

--- baselines/deepq/build_graph.py
+++ baselines/deepq/build_graph.py
@@ -413,6 +413,77 @@ def build_act_ltl(make_obs_ph,
         return act
 
 
+def build_act_ltl_expectation(make_obs_ph,
+                  q_func,
+                  num_actions,
+                  num_task_states, scope="deepq", reuse=None):
+    """Creates the act function:
+
+    Parameters
+    ----------
+    make_obs_ph: str -> tf.placeholder or TfInput
+        a function that take a name and creates a placeholder of input with that name
+    q_func: (tf.Variable, int, str, bool) -> tf.Variable
+        the model that takes the following inputs:
+            observation_in: object
+                the output of observation placeholder
+            num_actions: int
+                number of actions
+            scope: str
+            reuse: bool
+                should be passed to outer variable scope
+        and returns a tensor of shape (batch_size, num_actions) with values of every action.
+    num_actions: int
+        number of actions.
+    num_task_states: int
+        number of states in task MDP
+    scope: str or VariableScope
+        optional scope for variable_scope.
+    reuse: bool or None
+        whether or not the variables should be reused. To be able to reuse the scope must be given.
+
+    Returns
+    -------
+    act: (tf.Variable, bool, float) -> tf.Variable
+        function to select and action given observation.
+`       See the top of the file for details.
+    """
+    with tf.variable_scope(scope, reuse=reuse):
+        observations_ph = U.ensure_tf_input(make_obs_ph("observation"))
+        stochastic_ph = tf.placeholder(tf.bool, (), name="stochastic")
+        update_eps_ph = tf.placeholder(tf.float32, (), name="update_eps")
+        next_task_probs_ph = tf.placeholder(tf.float32, [num_task_states], name="next_task_probs")
+        transition_mat_ph = tf.placeholder(tf.float32, [num_task_states, num_task_states], name="trans_mat_train")
+
+        eps = tf.get_variable("eps", (), initializer=tf.constant_initializer(0))
+
+        # TODO do I have to fix something here? yes.
+        q_values = q_func(observations_ph.get(), num_actions, scope="q_func")
+        # q_values shape: [-1, num_task_states, num_actions] # TODO should it be -1?
+        q_values = tf.reshape(q_values, [-1, num_task_states, num_actions])
+        print("q_values:", q_values.shape)
+
+        # expected_q_values_across_states shape: [1, num_actions] #TODO  figure out the actual matrix mult.
+        expected_q_values_across_states = tf.tensordot(next_task_probs_ph, q_values, axes=[0, 1])
+
+        # if reshaped, q_values would be [-1, num_task_states, num_actions]
+        deterministic_actions = tf.argmax(expected_q_values_across_states, axis=-1)
+
+        batch_size = tf.shape(observations_ph.get())[0]
+        random_actions = tf.random_uniform(tf.stack([batch_size]), minval=0, maxval=num_actions, dtype=tf.int64)
+        chose_random = tf.random_uniform(tf.stack([batch_size]), minval=0, maxval=1, dtype=tf.float32) < eps
+        stochastic_actions = tf.where(chose_random, random_actions, deterministic_actions)
+
+        output_actions = tf.cond(stochastic_ph, lambda: stochastic_actions, lambda: deterministic_actions)
+        update_eps_expr = eps.assign(tf.cond(update_eps_ph >= 0, lambda: update_eps_ph, lambda: eps))
+
+        probs_next_task_state = tf.tensordot(next_task_probs_ph, transition_mat_ph, axes=1)
+        act = U.function(inputs=[observations_ph, stochastic_ph, update_eps_ph, next_task_probs_ph, transition_mat_ph],
+                         outputs=[output_actions, probs_next_task_state, update_eps_expr, eps],
+                         givens={update_eps_ph: -1.0, stochastic_ph: True},
+                         updates=[update_eps_expr])
+        return act
+
 def build_act_ltl_test_expectation(make_obs_ph, q_func, num_actions,
                        num_task_states, scope="deepq", reuse=None):
     """Creates the act function:
@@ -477,7 +548,6 @@ def build_act_ltl_test_expectation(make_obs_ph, q_func, num_actions,
         output_actions = tf.cond(stochastic_ph, lambda: stochastic_actions, lambda: deterministic_actions)
         update_eps_expr = eps.assign(tf.cond(update_eps_ph >= 0, lambda: update_eps_ph, lambda: eps))
 
-        # TODO check that this is the actual matrix mult
         probs_next_task_state = tf.tensordot(next_task_probs_ph, transition_mat_ph, axes=1)
         act = U.function(inputs=[observations_ph, stochastic_ph, update_eps_ph, next_task_probs_ph, transition_mat_ph],
                          outputs=[output_actions, probs_next_task_state, update_eps_expr, eps],
@@ -748,7 +818,7 @@ def build_train(make_obs_ph, q_func, num_actions, optimizer, grad_norm_clipping=
         return act_f, train, update_target, {'q_values': q_values}
 
 
-def build_train_ltl(make_obs_ph, q_func, num_actions, num_task_states, optimizer, acc_index, grad_norm_clipping=None, gamma=1.0,
+def build_train_ltl(make_obs_ph, q_func, num_actions, num_task_states, optimizer, acc_index, action_sel="argmax", grad_norm_clipping=None, gamma=1.0,
                     double_q=True, scope="deepq", reuse=None, param_noise=False, param_noise_filter_func=None):
     """Creates the train function:
 
@@ -809,7 +879,16 @@ def build_train_ltl(make_obs_ph, q_func, num_actions, num_task_states, optimizer
         act_f = build_act_with_param_noise(make_obs_ph, q_func, num_actions, scope=scope, reuse=reuse,
                                            param_noise_filter_func=param_noise_filter_func)
     else:
-        act_f = build_act_ltl(make_obs_ph, q_func, num_actions, num_task_states, scope=scope, reuse=reuse)
+        # TODO refactor this to use enums.
+        if action_sel == "argmax":
+            act_f = build_act_ltl(make_obs_ph, q_func, num_actions, num_task_states, scope=scope, reuse=reuse)
+        elif action_sel == "sample":
+            act_f = build_act_ltl_sample(make_obs_ph, q_func, num_actions, num_task_states, scope=scope, reuse=reuse)
+        elif action_sel == "expectation":
+            act_f = build_act_ltl_expectation(make_obs_ph, q_func, num_actions, num_task_states, scope=scope, reuse=reuse)
+        else:
+            raise ValueError("action-sel arg must be either 'argmax', 'sample', or 'expectation'")
+
 
     with tf.variable_scope(scope, reuse=reuse):
         # set up placeholders
